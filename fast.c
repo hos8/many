a73b1a2edee0e94589c244d9763edd3c7afc9056
hos8 <jam39so@outlook.com>
2024-08-28 15:54:38 +0800

    Modify fast.c

--- fast.c
+++ fast.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# List also road exactly performance always good.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
