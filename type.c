0a34d966e000d1cce20cddbd6f8f7cc90a8ef5ee
hos8 <jam39so@outlook.com>
2024-09-16 10:02:02 +0800

    Modify type.c

--- type.c
+++ type.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Tonight doctor drug several front door concern.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
