c317185ff9ad1deb1ed437181dc87ed58dbe2cc4
hos8 <jam39so@outlook.com>
2024-09-29 13:33:51 +0800

    Modify will.cpp

--- will.cpp
+++ will.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Policy western question.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
