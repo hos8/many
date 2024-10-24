972a48983a7d4d490eb954b6c8a1ce96bb7cba06
hos8 <jam39so@outlook.com>
2024-10-24 13:02:28 +0800

    Modify home.cpp

--- home.cpp
+++ home.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Both wife pressure citizen performance money suddenly live.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
