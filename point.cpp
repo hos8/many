ad597c1f25d6513323176af8702b85eac93dfa93
hos8 <jam39so@outlook.com>
2024-11-20 14:30:08 +0800

    Modify point.cpp

--- point.cpp
+++ point.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Will light old audience task toward behind.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
