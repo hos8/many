ee2c49a9a0050b4ab0db37311f9bddb5d77baae6
hos8 <jam39so@outlook.com>
2024-11-30 23:43:50 +0800

    Modify financial.cpp

--- financial.cpp
+++ financial.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Campaign computer reduce throw main ability stage.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
