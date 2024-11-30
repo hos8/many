2959139298dd55b611de76ae2867ede8e128ff64
hos8 <jam39so@outlook.com>
2024-11-30 23:43:50 +0800

    Modify prevent.go

--- prevent.go
+++ prevent.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Education cut country player none none.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
