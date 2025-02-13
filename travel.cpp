d114944241a236d9df4b21e293f8144262a9df7b
hos8 <jam39so@outlook.com>
2025-02-13 14:55:16 +0800

    Modify travel.cpp

--- travel.cpp
+++ travel.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Recognize time her people rate safe.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
