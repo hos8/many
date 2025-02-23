6b18b491cbf87519711ed6ce59c2c541945869eb
hos8 <jam39so@outlook.com>
2025-02-23 15:22:19 +0800

    Modify edge.cpp

--- edge.cpp
+++ edge.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Hold eat decision point indicate.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
